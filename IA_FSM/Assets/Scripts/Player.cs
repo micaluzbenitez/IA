54aa75fd56f6462cb17639503dc4bc91fda660f7
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-08-11 00:05:29 -0300

    Organizated scene

--- IA_FSM/Assets/Scripts/Player.cs
+++ IA_FSM/Assets/Scripts/Player.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IDamageable
 {
+    [Header("Movement")]
+    [SerializeField] private float speed;
+
     [Header("HP")]
     [SerializeField] private float hp;
 
     [Header("Damage")]
     [SerializeField] private int damage;
+
+    private Vector3 movementVector = Vector3.zero;
+    private Rigidbody playerRigidbody = null;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.z = Input.GetAxis("Vertical");
+
+        playerRigidbody.velocity = movementVector.normalized * speed;
+    }
 
     public void TakeDamage(float damage)
     {       
