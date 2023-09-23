f8bce5655c3e0df5eef54636b6dac901e14395f9
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-09-23 18:55:06 -0300

    Revert "Added simply voronoi logic"
    This reverts commit 43d3a798dc2989129724d6e349583df153af7fb3.

--- IA_FSM/Assets/Scripts/VoronoiDiagram/Voronoi.cs
+++ IA_FSM/Assets/Scripts/VoronoiDiagram/Voronoi.cs
@@ -1,63 +1,9 @@
 using UnityEngine;
 
-public class Voronoi : MonoBehaviour
+namespace VoronoiDiagram
 {
-    public Vector2Int imageDim;
-    public int regionAmount;
-
-    private void Start()
+    public class Voronoi : MonoBehaviour
     {
-        GetComponent<SpriteRenderer>().sprite = Sprite.Create(GetDiagram(), new Rect(0, 0, imageDim.x, imageDim.y), Vector2.one * 0.5f);
-    }
-
-    private Texture2D GetDiagram()
-    {
-        Vector2Int[] centroids = new Vector2Int[regionAmount];
-        Color[] regions = new Color[regionAmount];
-
-        for (int i = 0; i < regionAmount; i++)
-        {
-            centroids[i] = new Vector2Int(Random.Range(0, imageDim.x), Random.Range(0, imageDim.y));
-            regions[i] = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
-        }
-
-        Color[] pixelColors = new Color[imageDim.x * imageDim.y];
-
-        for (int x = 0; x < imageDim.x; x++)
-        {
-            for (int y = 0; y < imageDim.y; y++)
-            {
-                int index = x * imageDim.x + y;
-                pixelColors[index] = regions[GetClosestCentroidIndex(new Vector2Int(x, y), centroids)];
-            }
-        }
-
-        return GetImageFromColorArray(pixelColors);
-    }
-
-    private int GetClosestCentroidIndex(Vector2Int pixelPos, Vector2Int[] centroids)
-    {
-        float smallestDst = float.MaxValue;
-        int index = 0;
-
-        for (int i = 0; i < centroids.Length; i++)
-        {
-            if (Vector2.Distance(pixelPos, centroids[i]) < smallestDst)
-            {
-                smallestDst = Vector2.Distance(pixelPos, centroids[i]);
-                index = i;
-            }
-        }
-
-        return index;
-    }
-
-    private Texture2D GetImageFromColorArray(Color[] pixelColors)
-    {
-        Texture2D tex = new Texture2D(imageDim.x, imageDim.y);
-        tex.filterMode = FilterMode.Point;
-        tex.SetPixels(pixelColors);
-        tex.Apply();
-        return tex;
+        
     }
 }
