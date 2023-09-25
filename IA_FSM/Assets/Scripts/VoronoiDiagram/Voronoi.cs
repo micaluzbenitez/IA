f9a2db893bda93dfef7264b0c3e757512c8288ef
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-09-24 23:34:06 -0300

    Added voronoi construction

--- IA_FSM/Assets/Scripts/VoronoiDiagram/Voronoi.cs
+++ IA_FSM/Assets/Scripts/VoronoiDiagram/Voronoi.cs
@@ -1,9 +1,92 @@
+using System.Collections.Generic;
 using UnityEngine;
+using RTSGame.Entities.Buildings;
+using RTSGame.Map;
 
 namespace VoronoiDiagram
 {
     public class Voronoi : MonoBehaviour
     {
-        
+        private List<Limit> limits = new List<Limit>();
+        private List<Sector> sectors = new List<Sector>();
+
+        public void Init()
+        {
+            InitLimits();
+        }
+
+        private void InitLimits()
+        {
+            // Calculo los limites del mapa con sus dimensiones, distancia entre nodos y punto de origen
+            Vector2 mapSize = MapGenerator.MapDimensions * MapGenerator.CellSize;
+            Vector2 offset = MapGenerator.OriginPosition;
+
+            limits.Add(new Limit(new Vector2(0f, mapSize.y) + offset, DIRECTION.UP));
+            limits.Add(new Limit(new Vector2(mapSize.x, 0f) + offset, DIRECTION.DOWN));
+
+            limits.Add(new Limit(new Vector2(mapSize.x, mapSize.y) + offset, DIRECTION.RIGHT));
+            limits.Add(new Limit(new Vector2(0f, 0f) + offset, DIRECTION.LEFT));
+        }
+
+        public void SetVoronoi(List<GoldMine> goldMines)
+        {
+            sectors.Clear();
+            if (goldMines.Count <= 0) return;
+
+            for (int i = 0; i < goldMines.Count; i++)
+            {
+                // Agrego las minas de oro como sectores
+                sectors.Add(new Sector(goldMines[i]));
+            }
+
+            for (int i = 0; i < sectors.Count; i++)
+            {
+                // Agrego los limites a cada sector
+                sectors[i].AddSegmentLimits(limits);
+            }
+
+            for (int i = 0; i < goldMines.Count; i++)
+            {
+                for (int j = 0; j < goldMines.Count; j++)
+                {
+                    // Agrego los segmentos entre cada sector (menos entre si mismo)
+                    if (i == j) continue;
+                    sectors[i].AddSegment(goldMines[i].transform.position, goldMines[j].transform.position);
+                }
+            }
+
+            for (int i = 0; i < sectors.Count; i++)
+            {
+                // Calculo las intersecciones
+                sectors[i].SetIntersections();
+            }
+        }
+
+        public GoldMine GetMineCloser(Vector3 agentPosition)
+        {
+            // Calculo que mina esta mas cerca a x position
+            if (sectors != null)
+            {
+                for (int i = 0; i < sectors.Count; i++)
+                {
+                    if (sectors[i].CheckPointInSector(agentPosition))
+                    {
+                        return sectors[i].Mine;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (sectors.Count <= 0) return;
+
+            for (int i = 0; i < sectors.Count; i++)
+            {
+                sectors[i].Draw();
+            }
+        }
     }
 }
