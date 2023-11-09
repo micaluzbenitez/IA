956cc85e12663131e5dd14024f33e3cea8b1ac87
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-11-08 21:13:26 -0300

    Changed UI

--- IA_Parcial2/Assets/Scripts/UI/SimulationScreen.cs
+++ IA_Parcial2/Assets/Scripts/UI/SimulationScreen.cs
@@ -1,63 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SimulationScreen : MonoBehaviour
 {
-    public Text generationsCountTxt;
-    public Text bestFitnessTxt;
-    public Text avgFitnessTxt;
-    public Text worstFitnessTxt;
-    public Text timerTxt;
-    public Slider timerSlider;
-    public Button pauseBtn;
-    public Button stopBtn;
-    public GameObject startConfigurationScreen;
+    public GameObject verticalLayout = null;
+    public TMP_Text generationsCountTxt = null;
+    public TMP_Text turnsLeftTxt = null;
+    public TMP_Text bestFitnessTxt = null;
+    public TMP_Text avgFitnessTxt = null;
+    public TMP_Text worstFitnessTxt = null;
+    public TMP_Text timerTxt = null;
+    public Slider timerSlider = null;
 
-    string generationsCountText;
-    string bestFitnessText;
-    string avgFitnessText;
-    string worstFitnessText;
-    string timerText;
-    int lastGeneration = 0;
+    [Header("Gameplay")]
+    public TMP_Text totalAgentsTxt = null;
+    public TMP_Text totalFoodsTxt = null;
+    public TMP_Text totalDeathsTxt = null;
+    public TMP_Text agentsATxt = null;
+    public TMP_Text foodsATxt = null;
+    public TMP_Text deathsATxt = null;
+    public TMP_Text extinctsATxt = null;
+    public TMP_Text agentsBTxt = null;
+    public TMP_Text foodsBTxt = null;
+    public TMP_Text deathsBTxt = null;
+    public TMP_Text extinctsBTxt = null;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Buttons")]
+    public Button pauseBtn = null;
+    public Button saveBtn = null;
+    public Button stopBtn = null;
+
+    string generationsCountText = "";
+    string turnsLeftText = "";
+    string bestFitnessText = "";
+    string avgFitnessText = "";
+    string worstFitnessText = "";
+    string timerText = "";
+
+    string totalAgentsText = "";
+    string totalFoodsText = "";
+    string totalDeathsText = "";
+    string agentsAText = "";
+    string foodsAText = "";
+    string deathsAText = "";
+    string extinctsAText = "";
+    string agentsBText = "";
+    string foodsBText = "";
+    string deathsBText = "";
+    string extinctsBText = "";
+
+    public void Init(Action onPauseGame, Action onStopSimulation)
     {
         timerSlider.onValueChanged.AddListener(OnTimerChange);
         timerText = timerTxt.text;
 
         timerTxt.text = string.Format(timerText, PopulationManager.Instance.IterationCount);
 
-        if (string.IsNullOrEmpty(generationsCountText))
-            generationsCountText = generationsCountTxt.text;   
-        if (string.IsNullOrEmpty(bestFitnessText))
-            bestFitnessText = bestFitnessTxt.text;   
-        if (string.IsNullOrEmpty(avgFitnessText))
-            avgFitnessText = avgFitnessTxt.text;   
-        if (string.IsNullOrEmpty(worstFitnessText))
-            worstFitnessText = worstFitnessTxt.text;   
+        if (string.IsNullOrEmpty(generationsCountText)) generationsCountText = generationsCountTxt.text;
+        if (string.IsNullOrEmpty(turnsLeftText)) turnsLeftText = turnsLeftTxt.text;
+        if (string.IsNullOrEmpty(bestFitnessText)) bestFitnessText = bestFitnessTxt.text;
+        if (string.IsNullOrEmpty(avgFitnessText)) avgFitnessText = avgFitnessTxt.text;
+        if (string.IsNullOrEmpty(worstFitnessText)) worstFitnessText = worstFitnessTxt.text;
 
-        pauseBtn.onClick.AddListener(OnPauseButtonClick);
-        stopBtn.onClick.AddListener(OnStopButtonClick);
-    }
+        if (string.IsNullOrEmpty(totalAgentsText)) totalAgentsText = totalAgentsTxt.text;
+        if (string.IsNullOrEmpty(totalFoodsText)) totalFoodsText = totalFoodsTxt.text;
+        if (string.IsNullOrEmpty(totalDeathsText)) totalDeathsText = totalDeathsTxt.text;
+        if (string.IsNullOrEmpty(agentsAText)) agentsAText = agentsATxt.text;
+        if (string.IsNullOrEmpty(foodsAText)) foodsAText = foodsATxt.text;
+        if (string.IsNullOrEmpty(deathsAText)) deathsAText = deathsATxt.text;
+        if (string.IsNullOrEmpty(extinctsAText)) extinctsAText = extinctsATxt.text;
+        if (string.IsNullOrEmpty(agentsBText)) agentsBText = agentsBTxt.text;
+        if (string.IsNullOrEmpty(foodsBText)) foodsBText = foodsBTxt.text;
+        if (string.IsNullOrEmpty(deathsBText)) deathsBText = deathsBTxt.text;
+        if (string.IsNullOrEmpty(extinctsBText)) extinctsBText = extinctsBTxt.text;
 
-    void OnEnable()
-    {
-        if (string.IsNullOrEmpty(generationsCountText))
-            generationsCountText = generationsCountTxt.text;   
-        if (string.IsNullOrEmpty(bestFitnessText))
-            bestFitnessText = bestFitnessTxt.text;   
-        if (string.IsNullOrEmpty(avgFitnessText))
-            avgFitnessText = avgFitnessTxt.text;   
-        if (string.IsNullOrEmpty(worstFitnessText))
-            worstFitnessText = worstFitnessTxt.text;   
-
-        generationsCountTxt.text = string.Format(generationsCountText, 0);
-        bestFitnessTxt.text = string.Format(bestFitnessText, 0);
-        avgFitnessTxt.text = string.Format(avgFitnessText, 0);
-        worstFitnessTxt.text = string.Format(worstFitnessText, 0);
+        pauseBtn.onClick.AddListener(() => onPauseGame?.Invoke());
+        saveBtn.onClick.AddListener(() => PopulationManager.Instance.SaveData());
+        stopBtn.onClick.AddListener(() => onStopSimulation?.Invoke());
     }
 
     void OnTimerChange(float value)
@@ -66,28 +87,31 @@ public class SimulationScreen : MonoBehaviour
         timerTxt.text = string.Format(timerText, PopulationManager.Instance.IterationCount);
     }
 
-    void OnPauseButtonClick()
+    public void Active(bool status)
     {
-        PopulationManager.Instance.PauseSimulation();
-    }
-
-    void OnStopButtonClick()
-    {
-        PopulationManager.Instance.StopSimulation();
-        this.gameObject.SetActive(false);
-        startConfigurationScreen.SetActive(true);
-        lastGeneration = 0;
+        verticalLayout.SetActive(status);
     }
 
     void LateUpdate()
     {
-        if (lastGeneration != PopulationManager.Instance.generation)
-        {
-            lastGeneration = PopulationManager.Instance.generation;
-            generationsCountTxt.text = string.Format(generationsCountText, PopulationManager.Instance.generation);
-            bestFitnessTxt.text = string.Format(bestFitnessText, PopulationManager.Instance.bestFitness);
-            avgFitnessTxt.text = string.Format(avgFitnessText, PopulationManager.Instance.avgFitness);
-            worstFitnessTxt.text = string.Format(worstFitnessText, PopulationManager.Instance.worstFitness);
-        }
+        timerTxt.text = string.Format(timerText, PopulationManager.Instance.IterationCount);
+        generationsCountTxt.text = string.Format(generationsCountText, PopulationManager.Instance.generation);
+        turnsLeftTxt.text = string.Format(turnsLeftText, PopulationManager.Instance.turnsLeft);
+        bestFitnessTxt.text = string.Format(bestFitnessText, PopulationManager.Instance.bestFitness);
+        avgFitnessTxt.text = string.Format(avgFitnessText, PopulationManager.Instance.avgFitness);
+        worstFitnessTxt.text = string.Format(worstFitnessText, PopulationManager.Instance.worstFitness);
+
+        totalAgentsTxt.text = string.Format(totalAgentsText, PopulationManager.Instance.totalAgents);
+        totalFoodsTxt.text = string.Format(totalFoodsText, PopulationManager.Instance.totalFoodsConsumed);
+        totalDeathsTxt.text = string.Format(totalDeathsText, PopulationManager.Instance.totalDeaths);
+        agentsATxt.text = string.Format(agentsAText, PopulationManager.Instance.teams[0].agents);
+        foodsATxt.text = string.Format(foodsAText, PopulationManager.Instance.teams[0].foods);
+        deathsATxt.text = string.Format(deathsAText, PopulationManager.Instance.teams[0].deaths);
+        extinctsATxt.text = string.Format(extinctsAText, PopulationManager.Instance.teams[0].extincts);
+        agentsBTxt.text = string.Format(agentsBText, PopulationManager.Instance.teams[1].agents);
+        foodsBTxt.text = string.Format(foodsBText, PopulationManager.Instance.teams[1].foods);
+        deathsBTxt.text = string.Format(deathsBText, PopulationManager.Instance.teams[1].deaths);
+        extinctsBTxt.text = string.Format(extinctsBText, PopulationManager.Instance.teams[1].extincts);
     }
 }
+
--- IA_Parcial2/Assets/Scripts/UI/StartConfigurationScreen.cs
+++ IA_Parcial2/Assets/Scripts/UI/StartConfigurationScreen.cs
@@ -1,159 +1,271 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StartConfigurationScreen : MonoBehaviour
 {
-    public Text populationCountTxt;
-    public Slider populationCountSlider;
-    public Text minesCountTxt;
-    public Slider minesCountSlider;
-    public Text generationDurationTxt;
-    public Slider generationDurationSlider;
-    public Text eliteCountTxt;
-    public Slider eliteCountSlider;
-    public Text mutationChanceTxt;
-    public Slider mutationChanceSlider;
-    public Text mutationRateTxt;
-    public Slider mutationRateSlider;
-    public Text hiddenLayersCountTxt;
-    public Slider hiddenLayersCountSlider;
-    public Text neuronsPerHLCountTxt;
-    public Slider neuronsPerHLSlider;
-    public Text biasTxt;
-    public Slider biasSlider;
-    public Text sigmoidSlopeTxt;
-    public Slider sigmoidSlopeSlider;
-    public Button startButton;
-    public GameObject simulationScreen;
-    
-    string populationText;
-    string minesText;
-    string generationDurationText;
-    string elitesText;
-    string mutationChanceText;
-    string mutationRateText;
-    string hiddenLayersCountText;
-    string biasText;
-    string sigmoidSlopeText;
-    string neuronsPerHLCountText;
+    [Serializable]
+    public class TeamsUI
+    {
+        public string Tag = "";
+        public TMP_Text eliteCountTxt = null;
+        public Slider eliteCountSlider = null;
+        public TMP_Text mutationChanceTxt = null;
+        public Slider mutationChanceSlider = null;
+        public TMP_Text mutationRateTxt = null;
+        public Slider mutationRateSlider = null;
+        public TMP_Text hiddenLayersCountTxt = null;
+        public Slider hiddenLayersCountSlider = null;
+        public TMP_Text neuronsPerHLCountTxt = null;
+        public Slider neuronsPerHLSlider = null;
+        public TMP_Text biasTxt = null;
+        public Slider biasSlider = null;
+        public TMP_Text sigmoidSlopeTxt = null;
+        public Slider sigmoidSlopeSlider = null;
 
-    void Start()
-    {   
+        [HideInInspector] public string elitesText = "";
+        [HideInInspector] public string mutationChanceText = "";
+        [HideInInspector] public string mutationRateText = "";
+        [HideInInspector] public string hiddenLayersCountText = "";
+        [HideInInspector] public string biasText = "";
+        [HideInInspector] public string sigmoidSlopeText = "";
+        [HideInInspector] public string neuronsPerHLCountText = "";
+    }
+
+    public GameObject verticalLayout = null;
+    public TMP_Text populationCountTxt = null;
+    public Slider populationCountSlider = null;
+    public TMP_Text turnsTxt = null;
+    public Slider turnsSlider = null;
+    public TMP_Text agentMaxGenerationTxt = null;
+    public Slider agentMaxGenerationSlider = null; 
+    public TMP_Text deathChanceTxt = null;
+    public Slider deathChanceSlider = null;
+    public TMP_Text plusMutationRateTxt = null;
+    public Slider plusMutationRateSlider = null;
+
+    [Header("Teams")]
+    public TeamsUI[] teamsUI = null;
+
+    [Header("Buttons")]
+    [SerializeField] private Button startButton = null;
+    [SerializeField] private Button loadButton = null;
+
+    string populationText = "";
+    string turnsText = "";
+    string agentMaxGenerationText = "";
+    string deathChanceText = "";
+    string plusMutationRateText = "";
+
+    public void Init(Action<bool> onStartGame)
+    {
         populationCountSlider.onValueChanged.AddListener(OnPopulationCountChange);
-        minesCountSlider.onValueChanged.AddListener(OnMinesCountChange);
-        generationDurationSlider.onValueChanged.AddListener(OnGenerationDurationChange);
-        eliteCountSlider.onValueChanged.AddListener(OnEliteCountChange);
-        mutationChanceSlider.onValueChanged.AddListener(OnMutationChanceChange);
-        mutationRateSlider.onValueChanged.AddListener(OnMutationRateChange);
-        hiddenLayersCountSlider.onValueChanged.AddListener(OnHiddenLayersCountChange);
-        neuronsPerHLSlider.onValueChanged.AddListener(OnNeuronsPerHLChange);
-        biasSlider.onValueChanged.AddListener(OnBiasChange);
-        sigmoidSlopeSlider.onValueChanged.AddListener(OnSigmoidSlopeChange);
+        turnsSlider.onValueChanged.AddListener(OnTurnsChange);
+        agentMaxGenerationSlider.onValueChanged.AddListener(OnAgentMaxGenerationChange);
+        deathChanceSlider.onValueChanged.AddListener(OnDeathChanceAChange);
+        plusMutationRateSlider.onValueChanged.AddListener(OnPlusMutationRateChange);
+
+        teamsUI[0].eliteCountSlider.onValueChanged.AddListener(OnEliteCountAChange);
+        teamsUI[0].mutationChanceSlider.onValueChanged.AddListener(OnMutationChanceAChange);
+        teamsUI[0].mutationRateSlider.onValueChanged.AddListener(OnMutationRateAChange);
+        teamsUI[0].hiddenLayersCountSlider.onValueChanged.AddListener(OnHiddenLayersCountAChange);
+        teamsUI[0].neuronsPerHLSlider.onValueChanged.AddListener(OnNeuronsPerHLAChange);
+        teamsUI[0].biasSlider.onValueChanged.AddListener(OnBiasAChange);
+        teamsUI[0].sigmoidSlopeSlider.onValueChanged.AddListener(OnSigmoidSlopeAChange);
+
+        teamsUI[1].eliteCountSlider.onValueChanged.AddListener(OnEliteCountBChange);
+        teamsUI[1].mutationChanceSlider.onValueChanged.AddListener(OnMutationChanceBChange);
+        teamsUI[1].mutationRateSlider.onValueChanged.AddListener(OnMutationRateBChange);
+        teamsUI[1].hiddenLayersCountSlider.onValueChanged.AddListener(OnHiddenLayersCountBChange);
+        teamsUI[1].neuronsPerHLSlider.onValueChanged.AddListener(OnNeuronsPerHLBChange);
+        teamsUI[1].biasSlider.onValueChanged.AddListener(OnBiasBChange);
+        teamsUI[1].sigmoidSlopeSlider.onValueChanged.AddListener(OnSigmoidSlopeBChange);
 
         populationText = populationCountTxt.text;
-        minesText = minesCountTxt.text;
-        generationDurationText = generationDurationTxt.text;
-        elitesText = eliteCountTxt.text;
-        mutationChanceText = mutationChanceTxt.text;
-        mutationRateText = mutationRateTxt.text;
-        hiddenLayersCountText = hiddenLayersCountTxt.text;
-        neuronsPerHLCountText = neuronsPerHLCountTxt.text;
-        biasText = biasTxt.text;
-        sigmoidSlopeText = sigmoidSlopeTxt.text;
+        turnsText = turnsTxt.text;
+        agentMaxGenerationText = agentMaxGenerationTxt.text;
+        deathChanceText = deathChanceTxt.text;
+        plusMutationRateText = plusMutationRateTxt.text;
+
+        teamsUI[0].elitesText = teamsUI[0].eliteCountTxt.text;
+        teamsUI[0].mutationChanceText = teamsUI[0].mutationChanceTxt.text;
+        teamsUI[0].mutationRateText = teamsUI[0].mutationRateTxt.text;
+        teamsUI[0].hiddenLayersCountText = teamsUI[0].hiddenLayersCountTxt.text;
+        teamsUI[0].neuronsPerHLCountText = teamsUI[0].neuronsPerHLCountTxt.text;
+        teamsUI[0].biasText = teamsUI[0].biasTxt.text;
+        teamsUI[0].sigmoidSlopeText = teamsUI[0].sigmoidSlopeTxt.text;
+
+        teamsUI[1].elitesText = teamsUI[1].eliteCountTxt.text;
+        teamsUI[1].mutationChanceText = teamsUI[1].mutationChanceTxt.text;
+        teamsUI[1].mutationRateText = teamsUI[1].mutationRateTxt.text;
+        teamsUI[1].hiddenLayersCountText = teamsUI[1].hiddenLayersCountTxt.text;
+        teamsUI[1].neuronsPerHLCountText = teamsUI[1].neuronsPerHLCountTxt.text;
+        teamsUI[1].biasText = teamsUI[1].biasTxt.text;
+        teamsUI[1].sigmoidSlopeText = teamsUI[1].sigmoidSlopeTxt.text;
 
         populationCountSlider.value = PopulationManager.Instance.PopulationCount;
-        minesCountSlider.value = PopulationManager.Instance.MinesCount;
-        generationDurationSlider.value = PopulationManager.Instance.GenerationDuration;
-        eliteCountSlider.value = PopulationManager.Instance.EliteCount;
-        mutationChanceSlider.value = PopulationManager.Instance.MutationChance * 100.0f;
-        mutationRateSlider.value = PopulationManager.Instance.MutationRate * 100.0f;
-        hiddenLayersCountSlider.value = PopulationManager.Instance.HiddenLayers;
-        neuronsPerHLSlider.value = PopulationManager.Instance.NeuronsCountPerHL;
-        biasSlider.value = -PopulationManager.Instance.Bias;
-        sigmoidSlopeSlider.value = PopulationManager.Instance.P;
+        turnsSlider.value = PopulationManager.Instance.Turns;
+        agentMaxGenerationSlider.value = PopulationManager.Instance.AgentMaxGeneration;
 
-        startButton.onClick.AddListener(OnStartButtonClick);        
+        teamsUI[0].eliteCountSlider.value = PopulationManager.Instance.teams[0].EliteCount;
+        teamsUI[0].mutationChanceSlider.value = PopulationManager.Instance.teams[0].MutationChance * 100.0f;
+        teamsUI[0].mutationRateSlider.value = PopulationManager.Instance.teams[0].MutationRate * 100.0f;
+        teamsUI[0].hiddenLayersCountSlider.value = PopulationManager.Instance.teams[0].HiddenLayers;
+        teamsUI[0].neuronsPerHLSlider.value = PopulationManager.Instance.teams[0].NeuronsCountPerHL;
+        teamsUI[0].biasSlider.value = PopulationManager.Instance.teams[0].Bias;
+        teamsUI[0].sigmoidSlopeSlider.value = PopulationManager.Instance.teams[0].P;
+
+        teamsUI[1].eliteCountSlider.value = PopulationManager.Instance.teams[1].EliteCount;
+        teamsUI[1].mutationChanceSlider.value = PopulationManager.Instance.teams[1].MutationChance * 100.0f;
+        teamsUI[1].mutationRateSlider.value = PopulationManager.Instance.teams[1].MutationRate * 100.0f;
+        teamsUI[1].hiddenLayersCountSlider.value = PopulationManager.Instance.teams[1].HiddenLayers;
+        teamsUI[1].neuronsPerHLSlider.value = PopulationManager.Instance.teams[1].NeuronsCountPerHL;
+        teamsUI[1].biasSlider.value = PopulationManager.Instance.teams[1].Bias;
+        teamsUI[1].sigmoidSlopeSlider.value = PopulationManager.Instance.teams[1].P;
+
+        OnPopulationCountChange(populationCountSlider.value);
+        OnTurnsChange(turnsSlider.value);
+        OnAgentMaxGenerationChange(agentMaxGenerationSlider.value);
+        OnDeathChanceAChange(deathChanceSlider.value);
+        OnPlusMutationRateChange(plusMutationRateSlider.value);
+
+        OnEliteCountAChange(teamsUI[0].eliteCountSlider.value);
+        OnMutationChanceAChange(teamsUI[0].mutationChanceSlider.value);
+        OnMutationRateAChange(teamsUI[0].mutationRateSlider.value);
+        OnHiddenLayersCountAChange(teamsUI[0].hiddenLayersCountSlider.value);
+        OnNeuronsPerHLAChange(teamsUI[0].neuronsPerHLSlider.value);
+        OnBiasAChange(teamsUI[0].biasSlider.value);
+        OnSigmoidSlopeAChange(teamsUI[0].sigmoidSlopeSlider.value);
+
+        OnEliteCountBChange(teamsUI[1].eliteCountSlider.value);
+        OnMutationChanceBChange(teamsUI[1].mutationChanceSlider.value);
+        OnMutationRateBChange(teamsUI[1].mutationRateSlider.value);
+        OnHiddenLayersCountBChange(teamsUI[1].hiddenLayersCountSlider.value);
+        OnNeuronsPerHLBChange(teamsUI[1].neuronsPerHLSlider.value);
+        OnBiasBChange(teamsUI[1].biasSlider.value);
+        OnSigmoidSlopeBChange(teamsUI[1].sigmoidSlopeSlider.value);
+
+        startButton.onClick.AddListener(() => { onStartGame?.Invoke(false); });
+        loadButton.onClick.AddListener(() => PopulationManager.Instance.LoadData(onStartGame));
     }
 
     void OnPopulationCountChange(float value)
     {
         PopulationManager.Instance.PopulationCount = (int)value;
-        
         populationCountTxt.text = string.Format(populationText, PopulationManager.Instance.PopulationCount);
     }
 
-    void OnMinesCountChange(float value)
+    void OnTurnsChange(float value)
     {
-        PopulationManager.Instance.MinesCount = (int)value;        
-
-        minesCountTxt.text = string.Format(minesText, PopulationManager.Instance.MinesCount);
+        PopulationManager.Instance.Turns = (int)value;
+        turnsTxt.text = string.Format(turnsText, PopulationManager.Instance.Turns);
     }
 
-    void OnGenerationDurationChange(float value)
+    void OnAgentMaxGenerationChange(float value)
     {
-        PopulationManager.Instance.GenerationDuration = (int)value;
-        
-        generationDurationTxt.text = string.Format(generationDurationText, PopulationManager.Instance.GenerationDuration);
+        PopulationManager.Instance.AgentMaxGeneration = (int)value;
+        agentMaxGenerationTxt.text = string.Format(agentMaxGenerationText, PopulationManager.Instance.AgentMaxGeneration);
     }
 
-    void OnEliteCountChange(float value)
+    void OnDeathChanceAChange(float value)
     {
-        PopulationManager.Instance.EliteCount = (int)value;
-
-        eliteCountTxt.text = string.Format(elitesText, PopulationManager.Instance.EliteCount);
+        PopulationManager.Instance.DeathChance = value / 100.0f;
+        deathChanceTxt.text = string.Format(deathChanceText, (int)(PopulationManager.Instance.DeathChance * 100));
     }
 
-    void OnMutationChanceChange(float value)
+    void OnPlusMutationRateChange(float value)
     {
-        PopulationManager.Instance.MutationChance = value / 100.0f;
-
-        mutationChanceTxt.text = string.Format(mutationChanceText, (int)(PopulationManager.Instance.MutationChance * 100));
+        PopulationManager.Instance.PlusMutationRate = value / 10.0f;
+        plusMutationRateTxt.text = string.Format(plusMutationRateText, (int)(PopulationManager.Instance.PlusMutationRate * 10));
     }
 
-    void OnMutationRateChange(float value)
+    void OnEliteCountAChange(float value)
     {
-        PopulationManager.Instance.MutationRate = value / 100.0f;
-
-        mutationRateTxt.text = string.Format(mutationRateText, (int)(PopulationManager.Instance.MutationRate * 100));
+        PopulationManager.Instance.teams[0].EliteCount = (int)value;
+        teamsUI[0].eliteCountTxt.text = string.Format(teamsUI[0].elitesText, PopulationManager.Instance.teams[0].EliteCount);
     }
 
-    void OnHiddenLayersCountChange(float value)
+    void OnMutationChanceAChange(float value)
     {
-        PopulationManager.Instance.HiddenLayers = (int)value;
-        
-
-        hiddenLayersCountTxt.text = string.Format(hiddenLayersCountText, PopulationManager.Instance.HiddenLayers);
+        PopulationManager.Instance.teams[0].MutationChance = value / 100.0f;
+        teamsUI[0].mutationChanceTxt.text = string.Format(teamsUI[0].mutationChanceText, (int)(PopulationManager.Instance.teams[0].MutationChance * 100));
     }
 
-    void OnNeuronsPerHLChange(float value)
+    void OnMutationRateAChange(float value)
     {
-        PopulationManager.Instance.NeuronsCountPerHL = (int)value;
-
-        neuronsPerHLCountTxt.text = string.Format(neuronsPerHLCountText, PopulationManager.Instance.NeuronsCountPerHL);
+        PopulationManager.Instance.teams[0].MutationRate = value / 100.0f;
+        teamsUI[0].mutationRateTxt.text = string.Format(teamsUI[0].mutationRateText, (int)(PopulationManager.Instance.teams[0].MutationRate * 100));
     }
 
-    void OnBiasChange(float value)
+    void OnHiddenLayersCountAChange(float value)
     {
-        PopulationManager.Instance.Bias = -value;
-
-        biasTxt.text = string.Format(biasText, PopulationManager.Instance.Bias.ToString("0.00"));
+        PopulationManager.Instance.teams[0].HiddenLayers = (int)value;
+        teamsUI[0].hiddenLayersCountTxt.text = string.Format(teamsUI[0].hiddenLayersCountText, PopulationManager.Instance.teams[0].HiddenLayers);
     }
 
-    void OnSigmoidSlopeChange(float value)
+    void OnNeuronsPerHLAChange(float value)
     {
-        PopulationManager.Instance.P = value;
-
-        sigmoidSlopeTxt.text = string.Format(sigmoidSlopeText, PopulationManager.Instance.P.ToString("0.00"));
+        PopulationManager.Instance.teams[0].NeuronsCountPerHL = (int)value;
+        teamsUI[0].neuronsPerHLCountTxt.text = string.Format(teamsUI[0].neuronsPerHLCountText, PopulationManager.Instance.teams[0].NeuronsCountPerHL);
     }
 
-
-    void OnStartButtonClick()
+    void OnBiasAChange(float value)
     {
-        PopulationManager.Instance.StartSimulation();
-        this.gameObject.SetActive(false);
-        simulationScreen.SetActive(true);
+        PopulationManager.Instance.teams[0].Bias = value;
+        teamsUI[0].biasTxt.text = string.Format(teamsUI[0].biasText, PopulationManager.Instance.teams[0].Bias.ToString("0.00"));
     }
-    
+
+    void OnSigmoidSlopeAChange(float value)
+    {
+        PopulationManager.Instance.teams[0].P = value;
+        teamsUI[0].sigmoidSlopeTxt.text = string.Format(teamsUI[0].sigmoidSlopeText, PopulationManager.Instance.teams[0].P.ToString("0.00"));
+    }
+
+    void OnEliteCountBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].EliteCount = (int)value;
+        teamsUI[1].eliteCountTxt.text = string.Format(teamsUI[1].elitesText, PopulationManager.Instance.teams[1].EliteCount);
+    }
+
+    void OnMutationChanceBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].MutationChance = value / 100.0f;
+        teamsUI[1].mutationChanceTxt.text = string.Format(teamsUI[1].mutationChanceText, (int)(PopulationManager.Instance.teams[1].MutationChance * 100));
+    }
+
+    void OnMutationRateBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].MutationRate = value / 100.0f;
+        teamsUI[1].mutationRateTxt.text = string.Format(teamsUI[1].mutationRateText, (int)(PopulationManager.Instance.teams[1].MutationRate * 100));
+    }
+
+    void OnHiddenLayersCountBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].HiddenLayers = (int)value;
+        teamsUI[1].hiddenLayersCountTxt.text = string.Format(teamsUI[1].hiddenLayersCountText, PopulationManager.Instance.teams[1].HiddenLayers);
+    }
+
+    void OnNeuronsPerHLBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].NeuronsCountPerHL = (int)value;
+        teamsUI[1].neuronsPerHLCountTxt.text = string.Format(teamsUI[1].neuronsPerHLCountText, PopulationManager.Instance.teams[1].NeuronsCountPerHL);
+    }
+
+    void OnBiasBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].Bias = value;
+        teamsUI[1].biasTxt.text = string.Format(teamsUI[1].biasText, PopulationManager.Instance.teams[1].Bias.ToString("0.00"));
+    }
+
+    void OnSigmoidSlopeBChange(float value)
+    {
+        PopulationManager.Instance.teams[1].P = value;
+        teamsUI[1].sigmoidSlopeTxt.text = string.Format(teamsUI[1].sigmoidSlopeText, PopulationManager.Instance.teams[1].P.ToString("0.00"));
+    }
+
+    public void Active(bool status)
+    {
+        verticalLayout.SetActive(status);
+    }
 }
