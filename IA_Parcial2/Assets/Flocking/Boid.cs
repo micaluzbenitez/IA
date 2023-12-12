739e296e65736bc9a2015bf0a725f327d9481590
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-12-11 23:25:19 -0300

    Improved flocking system

--- IA_Parcial2/Assets/Flocking/Boid.cs
+++ IA_Parcial2/Assets/Flocking/Boid.cs
@@ -9,6 +9,20 @@ namespace Flocking
         public Vector2 currentPosition;
         public CircleCollider2D circleCollider2D;
 
+        [Header("Distances")]
+        public float alignmentDistance = 0;
+        public float cohesionDistance = 0;
+        public float separationDistance = 0;
+        public float obstacleDistance = 0;
+        // Definen las distancias maximas a las que los boids interactuan con sus vecinos en los diferentes comportamientos
+
+        [Header("Weights")]
+        public float alignmentWeight = 0;
+        public float cohesionWeight = 0;
+        public float separationWeight = 0;
+        public float obstacleWeight = 0;
+        // Pesos para ajustar que tanto influye cada comportamiento en el flocking
+
         private FlockingManager fM;
 
         private void Start()
@@ -25,7 +39,10 @@ namespace Flocking
 
         public Vector2 ACS()
         {
-            Vector2 ACS = fM.Alignment(this) + fM.Cohesion(this) + fM.Separation(this) + fM.Direction(this, fM.flockPoint);
+            Vector2 ACS = fM.Alignment(this) * alignmentWeight + fM.Cohesion(this) * cohesionWeight +
+                          fM.Separation(this) * separationWeight + fM.Direction(this, fM.flockPoint) +
+                          fM.Obstacle(this) * obstacleWeight;
+
             ACS.Normalize();
 
             return ACS;
--- IA_Parcial2/Assets/Flocking/FlockingManager.cs
+++ IA_Parcial2/Assets/Flocking/FlockingManager.cs
@@ -5,8 +5,21 @@ namespace Flocking
 {
     public class FlockingManager : MonoBehaviour
     {
+        public enum CheckType // Comportamientos
+        { 
+            Alignment, 
+            Cohesion, 
+            Separation, 
+            Obstacle
+        }
+
         public static FlockingManager instance;
+
+        [Header("Target")]
         public GameObject flockPoint;
+
+        [Header("Obstacles")]
+        public Transform[] flockObstacles;
 
         private Boid[] boids;
 
@@ -20,29 +33,32 @@ namespace Flocking
             boids = FindObjectsOfType<Boid>();
         }
 
-        // Hacia donde miran
+        // Devuelve la direccion hacia donde miran los boids cercanos al boid dado
         public Vector2 Alignment(Boid boid)
         {
-            // Cada uno de los boids toma a todas las entidades que tiene en un radio
-            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+            // El boid dado toma a todas las entidades que tiene en su radio
+            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid, CheckType.Alignment);
             Vector2 avg = Vector2.zero;
 
-            // Toma hacia donde mira cada uno de ellos, calcula un promedio y lo normaliza
+            // Toma hacia donde mira cada uno de ellos
             foreach (Boid b in insideRadiusBoids)
             {
                 avg += (Vector2)b.transform.up.normalized;
             }
 
+            // Calcula un promedio y lo normaliza
             avg /= insideRadiusBoids.Count;
             avg.Normalize();
-            return avg;
+
+            // Devuelve la direccion promedio
+            return avg; 
         }
 
         // Cuan alineado esta a sus compañeros, cuan en conjunto va con el resto
         public Vector2 Cohesion(Boid boid)
         {
-            // Cada uno de los boids toma a todas las entidades que tiene en un radio
-            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+            // El boid dado toma a todas las entidades que tiene en su radio
+            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid, CheckType.Cohesion);
             Vector2 avg = Vector2.zero;
 
             // Calcula un promedio de su posicion
@@ -50,44 +66,46 @@ namespace Flocking
             {
                 avg += b.currentPosition;
             }
-
             avg /= insideRadiusBoids.Count;
 
             // Y la normaliza sacando sacando la posicion relativa a la suya
-            return (avg - boid.currentPosition).normalized;
+            // Devuelve la direccion hacia la posicion promedio de los boids cercanos
+            return (avg - boid.currentPosition).normalized; 
         }
 
-        // Cuanto no se pisan con el resto
+        // Cuanto no se pisan con el resto, evita colisiones
         public Vector2 Separation(Boid boid)
         {
-            // Cada uno de los boids toma a todas las entidades que tiene en un radio
-            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid);
+            // El boid dado toma a todas las entidades que tiene en su radio
+            List<Boid> insideRadiusBoids = GetInsideRadiusBoids(boid, CheckType.Separation);
             Vector2 avg = Vector2.zero;
 
             // Los suma
             foreach (Boid b in insideRadiusBoids)
             {
-                avg += (b.currentPosition - boid.currentPosition);
-
+                avg += b.currentPosition - boid.currentPosition;
             }
 
             // Los separa, hace un promedio y lo normaliza
             avg /= insideRadiusBoids.Count;
             avg *= -1;
             avg.Normalize();
-            return avg;
+
+            // Devuelve la direccion opuesta al promedio de las posiciones de los boids cercanos
+            return avg; 
         }
 
         // Hacia donde van
         public Vector2 Direction(Boid boid, GameObject target)
         {
-
             // parametro target = hacia donde mira
             // Suma hacia donde va
+            // Devuelve la direecion hacia el target
             return ((Vector2)target.transform.position - boid.currentPosition).normalized;
         }
 
-        public List<Boid> GetInsideRadiusBoids(Boid boid)
+        // Devuelve la lista de boids que estan dentro del radio dado, segun el comportamiento
+        public List<Boid> GetInsideRadiusBoids(Boid boid, CheckType checkType)
         {
             List<Boid> insideRadiusBoids = new List<Boid>();
 
@@ -95,7 +113,62 @@ namespace Flocking
             {
                 if (boid.circleCollider2D.OverlapPoint(b.currentPosition))
                 {
-                    insideRadiusBoids.Add(b);
+                    float distance = Vector2.Distance(b.currentPosition, boid.currentPosition);
+                    float maxDistance = 0;
+
+                    switch (checkType)
+                    {
+                        case CheckType.Alignment:
+                            maxDistance = boid.alignmentDistance;
+                            break;
+                        case CheckType.Cohesion:
+                            maxDistance = boid.cohesionDistance;
+                            break;
+                        case CheckType.Separation:
+                            maxDistance = boid.separationDistance;
+                            break;
+                    }
+
+                    if (distance <= maxDistance) insideRadiusBoids.Add(b);
+                }
+            }
+
+            return insideRadiusBoids;
+        }
+
+        // Cuanto no se pisa con los obstaculos, evita colisiones
+        public Vector2 Obstacle(Boid boid)
+        {
+            // El boid dado toma a todos los obstaculos que tiene en su radio
+            List<Vector2> insideRadiusBoids = GetInsideRadiusObstacles(boid);
+            Vector2 avg = Vector2.zero;
+
+            // Los suma
+            foreach (Vector2 b in insideRadiusBoids)
+            {
+                avg += b - boid.currentPosition;
+            }
+
+            // Los separa, hace un promedio y lo normaliza
+            avg /= insideRadiusBoids.Count;
+            avg *= -1;
+            avg.Normalize();
+
+            // Devulve la direccion opuesta al promedio de las posiciones de los obstaculos cercanos
+            return avg;
+        }
+
+        // Devuelve la lista de obstaculos que estan dentro del radio dado
+        public List<Vector2> GetInsideRadiusObstacles(Boid boid)
+        {
+            List<Vector2> insideRadiusBoids = new List<Vector2>();
+
+            foreach (Transform b in flockObstacles)
+            {
+                if (boid.circleCollider2D.OverlapPoint(b.position))
+                {
+                    float distance = Vector2.Distance(b.position, boid.currentPosition);
+                    if (distance <= boid.obstacleDistance) insideRadiusBoids.Add(b.position);
                 }
             }
 
